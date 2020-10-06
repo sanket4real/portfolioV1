fb9a82a32e9e10b51f2bffc2077eeb6a26eaa28b
desaisanket68@gmail.com <desaisanket68@gmail.com>
2020-10-06 17:26:20 +0530

    Added carousel, type-writer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 //components
 import MyNavbar from "./components/my-navbar/navbar.component";
+import MyCarousel from "./components/my-carousel/carousel.component";
+import TitleMessage from "./components/title-message/title-message.component";
+
 
 const App = () => {
   return (
     <div>
      <MyNavbar />
+     <MyCarousel />
+     <TitleMessage/>
+   
     </div>
   );
 }
