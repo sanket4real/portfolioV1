14dd8bea74b6fb0243d9e0a2d0dcda8a13d889f5
desaisanket68@gmail.com <desaisanket68@gmail.com>
2020-10-07 14:13:58 +0530

    added skills page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MyCarousel from "./components/my-carousel/carousel.component";
 import TitleMessage from "./components/title-message/title-message.component";
 import About from "./pages/About/about.component";
 import Container from 'react-bootstrap/Container';
+import Skills from "./pages/skills/skills.component";
 
 
 
@@ -32,6 +33,17 @@ const App = () => {
     </Container>
     </Parallax>
     </div>
+    {/* skills */}
+
+     <div>
+        <Container className="rounded d-flex justify-content-center"> 
+        <Fade duration={1500}>
+    <Skills />
+    </Fade>
+    </Container>
+    </div>
+
+    
     </div>
   );
 }
---
+++ src/pages/skills/skills-data.js
@@ -0,0 +1,115 @@
+import L_HTML5 from "../../assets/img/skills/html-5.svg";
+import L_CSS3 from "../../assets/img/skills/css3.svg";
+import L_SASS from "../../assets/img/skills/sass-1.svg";
+import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
+import L_REACT from "../../assets/img/skills/react.svg";
+import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
+import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
+import L_GIT from "../../assets/img/skills/git-icon.svg";
+import L_GITHUB_PAGES from "../../assets/img/skills/github.svg";
+import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
+
+
+export const skills = {
+  frontend: [
+    {
+      link: "https://www.w3schools.com/html/",
+      imgAltText: "HTML 5",
+      imgSrc: L_HTML5,
+      skillName: "HTML5",
+    },
+    {
+      link: "https://www.w3schools.com/css/default.asp",
+      imgAltText: "CSS 3",
+      imgSrc: L_CSS3,
+      skillName: "CSS3",
+    },
+    {
+      link: "https://www.w3schools.com/js/default.asp",
+      imgAltText: "JavaScript",
+      imgSrc: L_JAVASCRIPT,
+      skillName: "JavaScript",
+    },
+    {
+      link: "https://sass-lang.com/",
+      imgAltText: "Sass",
+      imgSrc: L_SASS,
+      skillName: "Sass",
+    },
+    {
+      link: "https://getbootstrap.com/",
+      imgAltText: "Bootstrap",
+      imgSrc: L_BOOTSTRAP,
+      skillName: "Bootstrap",
+    },
+    {
+      link: "https://reactjs.org/",
+      imgAltText: "React JS",
+      imgSrc: L_REACT,
+      skillName: "React JS",
+    },
+    {
+      link: "https://react-bootstrap.github.io/",
+      imgAltText: "React Bootstrap",
+      imgSrc: L_REACT_BOOTSTRAP,
+      skillName: "React Bootstrap",
+    },
+    {
+      link: "https://material-ui.com/",
+      imgAltText: "Material-UI",
+      imgSrc: L_MATERIALUI,
+      skillName: "Material-UI",
+    },
+  ],
+
+//   backend: [
+//     {
+//       link: "https://nodejs.org/en/",
+//       imgAltText: "Node.js",
+//       imgSrc: L_NODE_JS,
+//       skillName: "Node.js",
+//     },
+//     {
+//       link: "https://expressjs.com/",
+//       imgAltText: "Express",
+//       imgSrc: L_EXPRESS,
+//       skillName: "Express",
+//     },
+//     {
+//       link: "https://www.djangoproject.com/",
+//       imgAltText: "Django",
+//       imgSrc: L_DJANGO,
+//       skillName: "Django",
+//     },
+//     {
+//       link: "https://flask.palletsprojects.com/",
+//       imgAltText: "Flask",
+//       imgSrc: L_FLASK,
+//       skillName: "Flask",
+//     },
+//   ],
+  hostingPlatforms: [
+    {
+      link: "https://pages.github.com/",
+      imgAltText: "GitHub Pages",
+      imgSrc: L_GITHUB_PAGES,
+      skillName: "GitHub Pages",
+    }
+  ],
+  programmingLanguages: [
+    {
+      link: "https://www.javascript.com/",
+      imgAltText: "JavaScript",
+      imgSrc: L_JAVASCRIPT,
+      skillName: "JavaScript",
+    }
+  ],
+  versionControl: [
+    {
+      link: "https://git-scm.com/",
+      imgAltText: "GIT",
+      imgSrc: L_GIT,
+      skillName: "GIT",
+    },
+  ],
+};
